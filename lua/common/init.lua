8299857276c39a4a58bed6b2ddae0ea71e6e6d75
Max Niia <max.niia@external.epiroc.com>
2022-11-07 16:14:16 +0100

    Add profiles

---
+++ lua/common/init.lua
@@ -0,0 +1,4 @@
+require("common.keybindings")
+require("common.packages")
+require("common.options")
+
---
+++ lua/common/packages.lua
@@ -0,0 +1,16 @@
+local packageList = require("common.packageList")
+
+local paq = require("paq") 
+paq(packageList)
+
+require("nvim-web-devicons").setup()
+require("common.packages.gitsigns")
+require("common.packages.lspconfig")
+require("common.packages.telescope")
+require("common.packages.aerial")
+require("common.packages.tree")
+require("common.packages.treesitter")
+require("common.packages.bufferline")
+require("common.packages.catppuccin")
+require("common.packages.floaterm")
+require("common.packages.indentBlankLine")
