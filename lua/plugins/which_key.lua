23fa4a379bb82f4066635f5d0a64baddf253c4f1
Max Niia <maxemil_98@hotmail.com>
2023-02-18 20:57:53 +0100

    Add pre-commit

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -5,6 +5,18 @@ return {
 		config = function(_, _)
 			local wk = require("which-key")
 			wk.register({
+				d = {
+					function()
+						vim.lsp.buf.format({
+							async = true,
+						})
+					end,
+					"Format",
+				},
+				a = {
+					vim.lsp.buf.code_action,
+					"Apply fix",
+				},
 				c = {
 					"<cm:wqd>nohlsearch<CR>",
 					"Clear highlight",
@@ -12,6 +24,10 @@ return {
 				s = {
 					"<cmd>wall<CR>",
 					"Save all",
+				},
+				rn = {
+					vim.lsp.buf.rename,
+					"Rename",
 				},
 			}, {
 				prefix = "<leader>",
