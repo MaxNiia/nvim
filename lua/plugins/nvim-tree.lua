cbd3657f97ab8e5e726b163eefb8ca7be5df9a7b
Max Niia <maxemil_98@hotmail.com>
2023-02-18 22:19:11 +0100

    Add BarBar

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+			view = {
+				adaptive_size = true,
+				relativenumber = true,
+				number = true,
+			},
+			renderer = {
+				symlink_destination = false,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-tree").setup(opts)
+			local wk = require("which-key")
+			wk.register({
+				e = {
+					"<cmd>NvimTreeToggle<CR>",
+					"Explorer",
+				},
+				h = {
+					"<cmd>NvimTreeFindFile<CR>",
+					"Go to opened file",
+				},
+			}, {
+				prefix = "<leader>",
+			})
+		end,
+	},
+}
