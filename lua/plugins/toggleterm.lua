bbb4cf2e3b1ef74d5de36e8434e0ae4e55f3bb91
Max Niia <max.niia@external.epiroc.com>
2023-04-02 20:53:23 +0200

    Add minimum 80 columns to toggle term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -23,7 +23,7 @@ return {
 				if term.direction == "horizontal" then
 					return 15
 				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.4
+					return math.max(vim.o.columns * 0.25, 80)
 				end
 				return 20
 			end,
