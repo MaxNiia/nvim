76c4713eb54491227491c68d99890e259af81370
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Add inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,6 +11,28 @@ return {
 			"SmiteshP/nvim-navic",
 			"mfussenegger/nvim-dap",
 			"hrsh7th/nvim-cmp",
+			{
+				"lvimuser/lsp-inlayhints.nvim",
+				branch = "anticonceal";
+				opts = {
+				},
+				config = function(_, opts)
+					require("lsp-inlayhints").setup(opts)
+					vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+					vim.api.nvim_create_autocmd("LspAttach", {
+					  group = "LspAttach_inlayhints",
+					  callback = function(args)
+						if not (args.data and args.data.client_id) then
+						  return
+						end
+
+						local bufnr = args.buf
+						local client = vim.lsp.get_client_by_id(args.data.client_id)
+						require("lsp-inlayhints").on_attach(client, bufnr)
+					  end,
+					})
+				end,
+			}
 		},
 		opts = {
 			-- options for vim.diagnostic.config()
