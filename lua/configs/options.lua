66b7b4a4021e1f99839c236cfc076a16baeb01b0
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Use trouble instead of quickfix

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -85,3 +85,26 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
         vim.wo.spell=false
     end,
 })
+
+function ToggleTroubleAuto()
+    local buftype = "quickfix"
+    if vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0 then
+        buftype = "loclist"
+    end
+
+    local ok, trouble = pcall(require, "trouble")
+    if ok then
+        vim.api.nvim_win_close(0, true)
+        trouble.toggle(buftype)
+    else
+        local set = vim.opt_local
+        set.colorcolumn = ""
+        set.number = false
+        set.relativenumber = false
+        set.signcolumn = "no"
+    end
+end
+
+vim.cmd([[
+    autocmd BufWinEnter quickfix silent :lua ToggleTroubleAuto()
+]])
