03a23fdec3ac4f77ed2c9716bebe110ee45aaed4
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Remove duplicate navic lualine sections

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -282,15 +282,6 @@ return {
 					},
 				},
 				lualine_c = {
-					{
-						function()
-							return require("nvim-navic").get_location()
-						end,
-						cond = function()
-							return package.loaded["nvim-navic"]
-								and require("nvim-navic").is_available()
-						end,
-					},
 				},
 				lualine_x = {},
 				lualine_y = {},
