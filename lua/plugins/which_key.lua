5e155d434c803c4f2357de32ab7bbd0c8e8d51ef
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Add clear highlight keybind

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,37 +1,41 @@
 return {
-    {
-        "folke/which-key.nvim",
-        lazy = true,
-        config = function(_, _)
-           local wk = require("which-key")
-           wk.register({
-               d = {
-                   function()
-                      vim.lsp.buf.format({
-                          async = true,
-                      })
-                   end,
-                   "Format",
-               },
-               a = {
-                   vim.lsp.buf.code_action,
-                   "Apply fix",
-               },
-               rn = {
-                   vim.lsp.buf.rename,
-                   "Rename",
-               },
-               z = {
-                   "<cmd>set norelativenumber<CR>",
-                   "Relative OFF",
-               },
-               Z = {
-                   "<cmd>set relativenumber<CR>",
-                   "Relative ON",
-               },
-           }, {
-               prefix = "<leader>",
-           })
-        end,
-    },
+	{
+		"folke/which-key.nvim",
+		lazy = true,
+		config = function(_, _)
+			local wk = require("which-key")
+			wk.register({
+				d = {
+					function()
+						vim.lsp.buf.format({
+							async = true,
+						})
+					end,
+					"Format",
+				},
+				a = {
+					vim.lsp.buf.code_action,
+					"Apply fix",
+				},
+				rn = {
+					vim.lsp.buf.rename,
+					"Rename",
+				},
+				C = {
+					"<cmd>nohl<CR>",
+					"Clear highlighting",
+				},
+				z = {
+					"<cmd>set norelativenumber<CR>",
+					"Relative OFF",
+				},
+				Z = {
+					"<cmd>set relativenumber<CR>",
+					"Relative ON",
+				},
+			}, {
+				prefix = "<leader>",
+			})
+		end,
+	},
 }
