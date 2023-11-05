41b2e776478d40350121e6eb2a957529f62e89fa
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Add cmp sources

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -13,8 +13,9 @@ return {
 			"onsails/lspkind.nvim",
 			"SmiteshP/nvim-navic",
 			"mfussenegger/nvim-dap",
-			"hrsh7th/nvim-cmp",
 			"kevinhwang91/nvim-ufo",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		keys = {
 			{
@@ -132,7 +133,7 @@ return {
 					-- },
 					K = {
 						function()
-							local winid = require('ufo').peekFoldedLinesUnderCursor()
+							local winid = require("ufo").peekFoldedLinesUnderCursor()
 							if not winid then
 								-- choose one of coc.nvim and nvim lsp
 								require("pretty_hover").hover()
@@ -230,82 +231,6 @@ return {
 					end
 				end
 			end
-
-			-- luasnip setup
-			local luasnip = require("luasnip")
-
-			-- Lspkind
-			local lspkind = require("lspkind")
-
-			-- nvim-cmp setup
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					docuementation = cmp.config.window.bordered(),
-				},
-				formatting = {
-					format = function(entry, vim_item)
-						if vim.tbl_contains({ "path" }, entry.source.name) then
-							local icon, hl_group = require("nvim-web-devicons").get_icon(
-								entry:get_completion_item().label
-							)
-							if icon then
-								vim_item.kind = icon
-								vim_item.kind_hl_group = hl_group
-								return vim_item
-							end
-						end
-						return lspkind.cmp_format({ with_text = false })(entry, vim_item)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
-					["<tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_locally_jumpable() then
-							luasnip.expand_or_jump()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.locally_jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
-				sources = {
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "fuzzzy_buffer" },
-					{ name = "buffer" },
-				},
-			})
-
-			cmp.setup.cmdline("/", {
-				sources = cmp.config.sources({
-					{
-						name = "fuzzy_buffer",
-					},
-				}),
-			})
 
 			require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
 			require("mason-lspconfig").setup_handlers({ setup })
--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -53,5 +53,87 @@ return {
 			require("mini.pairs").setup(opts)
 		end,
 	},
-	require("plugins.mini.files"),
+	{
+		"echasnovski/mini.files",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		init = function()
+			-- Highlight Names
+			-- vim.api.nvim_set_hl(0, "MiniFilesBorder", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesBorderModified", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesDirectory", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesFile", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesNormal", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesTitle", { link = "" })
+			-- vim.api.nvim_set_hl(0, "MiniFilesTitleFocused", { link = "" })
+
+			-- Show dotfiles filter toggle.
+			local show_dotfiles = false
+
+			local filter_show = function(_)
+				return true
+			end
+
+			local filter_hide = function(fs_entry)
+				return not vim.startswith(fs_entry.name, ".")
+			end
+
+			local toggle_dotfiles = function()
+				show_dotfiles = not show_dotfiles
+				local new_filter = show_dotfiles and filter_show or filter_hide
+				MiniFiles.refresh({ content = { filter = new_filter } })
+			end
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "MiniFilesBufferCreate",
+				callback = function(args)
+					local buf_id = args.data.buf_id
+					-- Tweak left-hand side of mapping to your liking
+					vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+				end,
+			})
+		end,
+		version = false,
+		event = "BufEnter",
+		keys = {
+			{
+				"<leader>e",
+				"<cmd>lua MiniFiles.open(MiniFiles.get_latest_path())<cr>",
+				desc = "Files",
+				mode = "n",
+			},
+			{
+				"<leader>EC",
+				"<cmd>lua MiniFiles.open(nil, false)<cr>",
+				desc = "CWD",
+				mode = "n",
+			},
+			{
+				"<leader>EB",
+				"<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<cr>",
+				desc = "Buffer Dir",
+				mode = "n",
+			},
+			{
+				"<leader>EH",
+				"<cmd>lua MiniFiles.open(vim.fn.expand('$HOME'))<cr>",
+				desc = "Home",
+				mode = "n",
+			},
+		},
+		opts = {
+			windows = {
+				preview = true,
+			},
+			content = {
+				filter = function(fs_entry)
+					return not vim.startswith(fs_entry.name, ".")
+				end,
+			},
+		},
+		config = function(_, opts)
+			require("mini.files").setup(opts)
+		end,
+	},
 }
