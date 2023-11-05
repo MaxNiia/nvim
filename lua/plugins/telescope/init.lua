b5eb1f4f4dc35b27d85615aa8ee900965e917d2a
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Make persisted its own file

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -13,45 +13,7 @@ return {
                 "nvim-telescope/telescope-file-browser.nvim",
                 dependencies = { "nvim-lua/plenary.nvim" },
             },
-            {
-                -- TODO: Look at actually using.
-                "olimorris/persisted.nvim",
-                keys = {
-                    {
-                        "<leader>Wl",
-                        "<cmd>SessionLoad<cr>",
-                        desc = "Load session",
-                    },
-                    {
-                        "<leader>WL",
-                        "<cmd>SessionLoadLast<cr>",
-                        desc = "Load last session",
-                    },
-                    {
-                        "<leader>Wd",
-                        "<cmd>SessionStop<cr>",
-                        desc = "Don't save",
-                    },
-                },
-                opts = {
-                    use_git_branch = true,
-                    PersistedTelescopeLoadPre = function()
-                        vim.api.nvim_input("<ESC>:%bd<CR>")
-                    end,
-                    PersistedTelescopeLoadPost = function(session)
-                        print("Loaded session " .. session.name)
-                    end,
-                    picker_opts = {
-                        initial_mode = "normal",
-                        layout_strategy = "cursor",
-                        layout_config = ui.layouts.small_cursor,
-                        enable_preview = true,
-                    },
-                },
-                config = function(_, opts)
-                    require("persisted").setup(opts)
-                end,
-            },
+            "olimorris/persisted.nvim",
             "nvim-telescope/telescope-dap.nvim",
             "mfussenegger/nvim-dap",
             "ThePrimeagen/harpoon",
