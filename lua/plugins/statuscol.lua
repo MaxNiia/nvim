804383a9e8c03417046692a10a46b3f943323e7f
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Remove diagnostics from status col
    With inlay hints and trouble this functionality isn't really needed and just takes up space on the status column

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -38,17 +38,17 @@ return {
 						},
 						click = "v:lua.ScFa",
 					},
-					{
-						sign = {
-							name = {
-								"Diagnostic",
-							},
-							maxwidth = 2,
-							colwidth = 1,
-							auto = false,
-						},
-						click = "v:lua.ScSa",
-					},
+					-- {
+					-- 	sign = {
+					-- 		name = {
+					-- 			"Diagnostic",
+					-- 		},
+					-- 		maxwidth = 2,
+					-- 		colwidth = 1,
+					-- 		auto = false,
+					-- 	},
+					-- 	click = "v:lua.ScSa",
+					-- },
 					{
 						text = {
 							builtin.lnumfunc,
