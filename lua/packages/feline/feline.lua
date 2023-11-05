9fd5725fa76c591aaaef817570d5d3c67ce95799
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Add check for catppuccin in feline.lua

---
+++ lua/packages/feline/feline.lua
@@ -0,0 +1,6 @@
+-- TODO change true to some functionality for detecting color scheme.
+if (true) then
+   require("packages.feline.catp_feline")
+else
+   require("packages.feline.default_feline")
+end
