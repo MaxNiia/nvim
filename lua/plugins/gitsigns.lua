eee1662f49e5bc7cb593ac1ef4409172371ee855
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Add smartsplit

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,6 +4,10 @@ return {
 		lazy = true,
 		event = "BufEnter",
 		opts = {
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 			current_line_blame = true,
 			current_line_blame_opts = {
 				virt_text = true,
@@ -11,7 +15,7 @@ return {
 				delay = 1000,
 				ignore_whitespace = true,
 			},
-			current_line_blame_formatter = "<author>:<author_time:%Y:%m:%d> - <summary>",
+			current_line_blame_formatter = "<author>:<author_time:%Y:%m%d>-<summary>",
 		},
 		config = function(_, opts)
 			require("gitsigns").setup(opts)
