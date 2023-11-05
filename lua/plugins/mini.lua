13eaf2b733a8bb5724164d01c0b92d38396eab12
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Add mini

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,66 @@
+return {
+	{
+		"echasnovski/mini.animate",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.animate").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.ai").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.align",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.align").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.bracketed",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.bracketed").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.comment").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.cursorword",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.cursorword").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.jump",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.jump").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.jump2d",
+		version = false,
+		event = "BufEnter",
+		config = function()
+			require("mini.jump2d").setup()
+		end,
+	},
+}
