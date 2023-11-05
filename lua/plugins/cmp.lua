ac5878d473e0ca0053196143ba9431e566890bef
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Use upstream fzf

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,8 +4,8 @@ return {
 		lazy = true,
 		dependencies = {
 			{
-				"MaxNiia/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
 			},
 		},
 	},
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -290,8 +290,8 @@ return {
 		},
 		dependencies = {
 			{
-				"MaxNiia/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
 			},
 			{
 				"nvim-telescope/telescope-file-browser.nvim",
