474f84006b56795c95d29879062f41233e0f28c7
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Add diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,10 @@
+return {
+    {
+       "sindrets/diffview.nvim",
+        opts = {
+            diff_binaries = false,
+            enhanced_diff_hl = true,
+            use_icons = true,
+        },
+    },
+}
