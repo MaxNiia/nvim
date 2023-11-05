a7f22c527f6b2f1808607070b9e5da93b4226706
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Adapt to indent blankline v3

--- lua/plugins/indentBlankLine.lua
+++ lua/plugins/indentBlankLine.lua
@@ -1,18 +1,17 @@
 return {
     {
         "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
         lazy = true,
         event = { "BufReadPost", "BufNewFile" },
+        init = function()
+            vim.opt.list = true
+        end,
         opts = {
             char = "│",
             filetype_exclude = { "help", "alpha", "dashboard", "Nvim-tree", "Trouble", "lazy" },
             show_current_context = true,
             show_current_context_start = true,
         },
-        config = function(_, opts)
-            vim.opt.list = true
-
-            require("indent_blankline").setup(opts)
-        end,
     },
 }
