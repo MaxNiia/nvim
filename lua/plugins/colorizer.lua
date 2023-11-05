f43e691792ba7c6043c9c35e8c84ae8e42cf041e
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Add colorizer lua

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "norcalli/nvim-colorizer.lua",
+        main = "colorizer",
+        opts = {
+            "css",
+            "javascript",
+            "html",
+            "lua",
+        },
+    },
+}
