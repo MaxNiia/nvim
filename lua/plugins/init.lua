ac96dbf87e600d5c8ea911b18e8cd541cb932b16
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:51 +0100

    Fix small

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,20 @@ return {
 		},
 	},
 	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
+	{
 		"nvim-neorg/neorg",
 		ft = "norg",
 		config = true,
@@ -34,9 +48,33 @@ return {
 	},
 	{
 		"rcarriga/nvim-notify",
-		lazy = true,
-		event = "BufEnter",
-		config = true,
+		keys = {
+			{
+				"<leader>un",
+				function()
+					require("notify").dismiss({ silent = true, pending = true })
+				end,
+				desc = "Delete all Notifications",
+			},
+		},
+		opts = {
+			timeout = 3000,
+			max_height = function()
+				return math.floor(vim.o.lines * 0.75)
+			end,
+			max_width = function()
+				return math.floor(vim.o.columns * 0.75)
+			end,
+		},
+		init = function()
+			-- when noice is not enabled, install notify on VeryLazy
+			local Util = require("lazyvim.util")
+			if not Util.has("noice.nvim") then
+				Util.on_very_lazy(function()
+					vim.notify = require("notify")
+				end)
+			end
+		end,
 	},
 	{
 		"folke/todo-comments.nvim",
