3df2b2aaea25601160895411b8f26f1c18108899
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Mini.files options and add vertical/horizontal split

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -93,6 +93,33 @@ return {
                     vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
                 end,
             })
+
+            local map_split = function(buf_id, lhs, direction)
+                local rhs = function()
+                    -- Make new window and set it as target
+                    local new_target_window
+                    vim.api.nvim_win_call(MiniFiles.get_target_window() or 0, function()
+                        vim.cmd(direction .. " split")
+                        new_target_window = vim.api.nvim_get_current_win()
+                    end)
+
+                    MiniFiles.set_target_window(new_target_window)
+                end
+
+                -- Adding `desc` will result into `show_help` entries
+                local desc = "Split " .. direction
+                vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+            end
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    -- Tweak keys to your liking
+                    map_split(buf_id, "gs", "belowright horizontal")
+                    map_split(buf_id, "gv", "belowright vertical")
+                end,
+            })
         end,
         version = false,
         event = "BufEnter",
@@ -123,9 +150,13 @@ return {
             },
         },
         opts = {
-            options = {},
+            options = {
+                permament_delete = true,
+                use_as_default_explorer = true,
+            },
             windows = {
                 preview = true,
+                width_preview = 50,
             },
             content = {
                 filter = function(fs_entry)
