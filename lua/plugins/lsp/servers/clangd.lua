d9bfd0ee885038d24abf8d60a250fb924900abc5
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Add query driver picker

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -1,3 +1,15 @@
+local file_path = vim.fn.stdpath("cache") .. "/clangd.txt"
+local getQueryDriver = function()
+    local default = "/usr/bin/clang, /usr/bin/clang++"
+    local f = io.open(file_path, "r")
+    if f ~= nil then
+        local lines = f:read(2 ^ 10)
+        f:close()
+        return lines[1]
+    end
+    return default
+end
+
 return {
     filetypes = {
         "c",
@@ -12,7 +24,7 @@ return {
         "--clang-tidy",
         "--completion-style=detailed",
         "--all-scopes-completion=true",
-        "--query-driver='/usr/bin/clang, /usr/bin/clang++'",
+        "--query-driver=" .. getQueryDriver(),
         "--header-insertion=iwyu",
     },
 }
