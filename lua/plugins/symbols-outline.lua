46a2b8d852be35d8eca4c9180b86365d49671d54
Max Niia <muxinatorn@gmail.com>
2023-11-05 19:44:52 +0100

    Fix outline keybind

--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -3,9 +3,11 @@ return {
 		"simrat39/symbols-outline.nvim",
 		event = "VeryLazy",
 		keys = {
-			"<leader>S",
-			"<cmd>SymbolsOutline<cr>",
-			desc = "Symbolds Outline",
+			{
+				"<leader>S",
+				"<cmd>SymbolsOutline<cr>",
+				desc = "Symbolds Outline",
+			},
 		},
 		opts = {
 			position = "left",
