04cf7e3060879fa2ff9b08e6c93688e0d3826c0a
Max Niia <muxinatorn@gmail.com>
2024-02-13 21:33:43 +0100

    feat(git): Add keybind to commit

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,5 +1,18 @@
+local group_key = "g"
+if _G.neotree then
+    group_key = "H"
+end
+
 return { {
     "tpope/vim-fugitive",
+    keys = {
+        {
+            "<leader>".. group_key .. "c",
+                "<cmd>G commit<cr>",
+            desc = "Git commit",
+        },
+    
+    },
 }, {
     "cedarbaum/fugitive-azure-devops.vim",
 } }
