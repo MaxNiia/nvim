742cff9e30d87d35032bfd9945e2437e5a4538ee
Max Niia <muxinatorn@gmail.com>
2024-03-03 16:58:03 +0100

    feat(colorize): Switch to nvim-highligh-colors

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,42 +1,8 @@
 return {
     {
-        "NvChad/nvim-colorizer.lua",
-        lazy = true,
-        enabled = not _G.IS_VSCODE,
-        cmd = {
-            "ColorizerAttachToBuffer",
-            "ColorizerDetachFromBuffer",
-            "ColorizerToggle",
-            "ColorizerReloadAllBuffers",
-        },
-        main = "colorizer",
+        "brenoprata10/nvim-highlight-colors",
         opts = {
-            filetypes = {
-                "*",
-                cpp = { names = false },
-                c = { names = false },
-                cmp_docs = { always_update = true },
-            },
-            user_default_options = {
-                RRGGBBAA = true,
-                AARRGGBB = true,
-                mode = "background",
-                always_update = false,
-            },
+            enable_named_colors = true,
         },
-        config = function(_, opts)
-            local exclude_files = require("utils.exclude_files")
-
-            for _, file in ipairs(exclude_files) do
-                string.format("!%s", file)
-                opts = vim.tbl_deep_extend("force", {
-                    filetypes = {
-                        file,
-                    },
-                }, opts or {})
-            end
-
-            require("colorizer").setup(opts)
-        end,
     },
 }
