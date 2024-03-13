762894c5a9a0885e70529d7847b3e99e7e1e21ac
Max Niia <muxinatorn@gmail.com>
2024-03-13 20:48:07 +0100

    feat(colorizer): Remove named colors

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -2,7 +2,7 @@ return {
     {
         "brenoprata10/nvim-highlight-colors",
         opts = {
-            enable_named_colors = true,
+            enable_named_colors = false,
         },
     },
 }
