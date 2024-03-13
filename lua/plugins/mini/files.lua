3072b2159dfadfbaecfe09d77e71d52edfe8778c
Max Niia <muxinatorn@gmail.com>
2024-03-13 20:52:03 +0100

    feat(files): Add change dir keymap

--- lua/plugins/mini/files.lua
+++ lua/plugins/mini/files.lua
@@ -101,6 +101,20 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     init = function()
+        local files_set_cwd = function(path)
+            -- Works only if cursor is on the valid file system entry
+            local cur_entry_path = MiniFiles.get_fs_entry().path
+            local cur_directory = vim.fs.dirname(cur_entry_path)
+            vim.fn.chdir(cur_directory)
+        end
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+                vim.keymap.set("n", "g~", files_set_cwd, { buffer = args.data.buf_id })
+            end,
+        })
+
         vim.api.nvim_create_autocmd("User", {
             pattern = "MiniFilesBufferCreate",
             callback = function(args)
