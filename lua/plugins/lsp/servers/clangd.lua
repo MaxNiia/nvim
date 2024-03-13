52dca01d19d6476c7585f692861a2f369061b4eb
Max Niia <muxinatorn@gmail.com>
2024-03-13 18:59:56 +0100

    feat(windows): Hide linux specific things in clangd server

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -1,6 +1,9 @@
 local file_path = vim.fn.stdpath("cache") .. "/clangd.txt"
 local getQueryDriver = function()
     local default = "/usr/bin/clang, /usr/bin/clang++"
+    if _G.IS_WINDOWS and not _G.IS_WSL then
+         default = "gcc, g++"
+    end
     local f = io.open(file_path, "r")
     if f ~= nil then
         local lines = f:read(2 ^ 10)
@@ -10,6 +13,11 @@ local getQueryDriver = function()
         end
     end
     return default
+end
+
+local number_of_cores = 4
+if _G.IS_WINDOWS and not _G.IS_WSL then
+    number_of_cores = math.floor(tonumber(vim.fn.system("nproc")) * 0.75)
 end
 
 return {
@@ -22,11 +30,10 @@ return {
     },
     cmd = {
         "clangd",
-        "-j=" .. tostring(math.floor(tonumber(vim.fn.system("nproc")) * 0.75)),
+        "-j=" .. tostring(number_of_cores),
         "--background-index=true",
         "--clang-tidy",
         "--completion-style=detailed",
-        -- "--background-index-priority=low",
         "--malloc-trim",
         "--all-scopes-completion=true",
         "--query-driver=" .. getQueryDriver(),
