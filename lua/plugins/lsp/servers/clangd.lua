464012b7ccb14de04618ba6307d4f6e2941e16ec
Max Niia <muxinatorn@gmail.com>
2024-06-10 20:05:47 +0200

    fix: stop letting cmake tools choose compile commands

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -39,10 +39,4 @@ return {
         "--query-driver=" .. getQueryDriver(),
         "--header-insertion=iwyu",
     },
-    on_new_config = function(new_config, _)
-        local status, cmake = pcall(require, "cmake-tools")
-        if status then
-            cmake.clangd_on_new_config(new_config)
-        end
-    end,
 }
