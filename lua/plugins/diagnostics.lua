e6447724ecdedbec568564c3652b202f781b8d7a
Max Niia <muxinatorn@gmail.com>
2024-07-03 06:43:11 +0200

    fix: tiny inline highligts

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -41,25 +41,21 @@ return {
                 "TinyInlineDiagnosticVirtualTextHint",
                 { link = "DiagnosticVirtualTextHint" }
             )
-            vim.api.nvim_set_hl(0, "TinyInlineInvDiagnosticVirtualTextHint", {
+            vim.api.nvim_set_hl(0, "TinyInlineInvDiagnosticVirtualTextError", {
                 fg = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextError" })["bg"],
                 bg = vim.api.nvim_get_hl(0, { name = "CursorLine" })["bg"],
-                blend = 0,
             })
             vim.api.nvim_set_hl(0, "TinyInlineInvDiagnosticVirtualTextWarn", {
                 fg = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextWarn" })["bg"],
                 bg = vim.api.nvim_get_hl(0, { name = "CursorLine" })["bg"],
-                blend = 100,
             })
             vim.api.nvim_set_hl(0, "TinyInlineInvDiagnosticVirtualTextInfo", {
                 fg = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextInfo" })["bg"],
                 bg = vim.api.nvim_get_hl(0, { name = "CursorLine" })["bg"],
-                blend = 100,
             })
             vim.api.nvim_set_hl(0, "TinyInlineInvDiagnosticVirtualTextHint", {
                 fg = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextHint" })["bg"],
                 bg = vim.api.nvim_get_hl(0, { name = "CursorLine" })["bg"],
-                blend = 100,
             })
         end,
     },
