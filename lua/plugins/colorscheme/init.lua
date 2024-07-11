7f02efb6685633549e79ff72d4cc3c4d5ba116a4
Max Niia <muxinatorn@gmail.com>
2024-07-11 21:39:34 +0200

    chore: reorganize colorscheme files

---
+++ lua/plugins/colorscheme/init.lua
@@ -0,0 +1,6 @@
+return {
+    require("plugins.colorscheme.catppuccin"),
+    require("plugins.colorscheme.kanagawa"),
+    require("plugins.colorscheme.nightfox"),
+    require("plugins.colorscheme.tokyonight"),
+}
