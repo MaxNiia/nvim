db2f12af0c9317eb3f1cde0a4e96e3e10554ac37
Max Niia <muxinatorn@gmail.com>
2024-09-18 20:03:31 +0200

    feat: make markview behave better

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -5,13 +5,6 @@ return {
         ft = { "markdown" },
         opts = {
             modes = { "n", "no", "c" },
-            hybrid_modes = { "n" },
-            callbacks = {
-                on_enable = function(_, win)
-                    vim.wo[win].conceallevel = 2
-                    vim.wo[win].concealcursor = "c"
-                end,
-            },
         },
     },
 }
