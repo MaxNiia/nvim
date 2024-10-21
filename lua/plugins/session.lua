57f7aaeb866210e3fb6de03e62913f4b884769c0
Max Niia <muxinatorn@gmail.com>
2024-10-21 17:22:54 +0200

    chore: fix breaking changes in auto-save

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -29,15 +29,10 @@ return {
         end,
         opts = {
             enabled = true,
-            execution_message = {
-                message = nil,
-                dim = 0.18, -- dim the color of `message`
-                cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-            },
             trigger_events = { -- See :h events
                 immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
                 defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-                cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+                cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
             },
             write_all_buffers = true, -- write all buffers when the current one meets `condition`
             debounce_delay = 10000, -- saves the file at most every `debounce_delay` milliseconds
