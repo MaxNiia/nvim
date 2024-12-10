8074772a1944cf259a9cda79180e0d189c3a4c7b
Max Niia <muxinatorn@gmail.com>
2024-12-10 20:19:30 +0100

    feat: use snack indent instead of indent blankline

--- lua/niia/plugins/snacks.lua
+++ lua/niia/plugins/snacks.lua
@@ -83,6 +83,38 @@ return {
                 },
             },
             bigfile = { enabled = true },
+            indent = {},
+            indent = {
+                scope = {
+                    enabled = true,
+                    animate = {
+                        enabled = vim.fn.has("nvim-0.10") == 1,
+                        easing = "linear",
+                        duration = {
+                            step = 20, -- ms per step
+                            total = 400, -- maximum duration
+                        },
+                    },
+                    underline = false,
+                },
+                chunk = {
+                    -- when enabled, scopes will be rendered as chunks, except for the
+                    -- top-level scope which will be rendered as a scope.
+                    enabled = false,
+                    -- only show chunk scopes in the current window
+                    only_current = false,
+                    hl = "SnacksIndentChunk", ---@type string|string[] hl group for chunk scopes
+                    char = {
+                        corner_top = "┌",
+                        corner_bottom = "└",
+                        -- corner_top = "╭",
+                        -- corner_bottom = "╰",
+                        horizontal = "─",
+                        vertical = "│",
+                        arrow = ">",
+                    },
+                },
+            },
             dashboard = {
                 enabled = true,
                 preset = {
