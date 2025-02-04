5a105e5fd93f7741769944ed874023dec3e78e35
Max Niia <muxinatorn@gmail.com>
2025-02-04 21:57:42 +0100

    feat: add terminal keybindings

--- lua/niia/plugins/snacks.lua
+++ lua/niia/plugins/snacks.lua
@@ -126,6 +126,22 @@ return {
                     },
                 },
             },
+            terminal = {
+                keys = {
+                    q = "hide",
+                    gf = function(self)
+                        local f = vim.fn.findfile(vim.fn.expand("<cfile>"), "**")
+                        if f == "" then
+                            Snacks.notify.warn("No file under cursor")
+                        else
+                            self:hide()
+                            vim.schedule(function()
+                                vim.cmd("e " .. f)
+                            end)
+                        end
+                    end,
+                },
+            },
             indent = {
                 scope = {
                     enabled = true,
