44fe2469ddee3d467961b32b22bfd87d474a1a94
Max Niia <max.emil.niia@gmail.com>
2025-03-03 20:52:33 +0100

    feat: make baleia lazyload

--- lua/niia/plugins/init.lua
+++ lua/niia/plugins/init.lua
@@ -3,20 +3,16 @@ return {
     {
         "m00qek/baleia.nvim",
         version = "*",
-        config = function()
-            vim.g.baleia = require("baleia").setup({})
-
-            -- Command to colorize the current buffer
-            vim.api.nvim_create_user_command("BaleiaColorize", function()
+        lazy = true,
+        event = "VeryLazy",
+        cmd = "Colorize",
+        init = function()
+            vim.api.nvim_create_user_command("Colorize", function()
                 vim.g.baleia.once(vim.api.nvim_get_current_buf())
             end, { bang = true })
-
-            -- Command to show logs
-            vim.api.nvim_create_user_command(
-                "BaleiaLogs",
-                vim.g.baleia.logger.show,
-                { bang = true }
-            )
+        end,
+        config = function()
+            vim.g.baleia = require("baleia").setup({})
         end,
     },
     {
