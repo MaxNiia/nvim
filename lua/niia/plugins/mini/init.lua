a39aec04f04fe2946cfe959322b6e97172e73acd
Max Niia <max.emil.niia@gmail.com>
2025-03-03 21:06:22 +0100

    feat: use mini.nvim instead of individual repos

--- lua/niia/plugins/mini/init.lua
+++ lua/niia/plugins/mini/init.lua
@@ -1,106 +1,8 @@
 local file_functions = require("niia.plugins.mini.files")
 return {
     {
-        "echasnovski/mini.splitjoin",
-        version = "*",
-        config = true,
-    },
-    {
-        "echasnovski/mini.move",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            mappings = {
-                left = "<M-h>",
-                right = "<M-l>",
-                down = "<M-j>",
-                up = "<M-k>",
-
-                line_left = "<M-h>",
-                line_right = "<M-l>",
-                line_down = "<M-j>",
-                line_up = "<M-k>",
-            },
-        },
-        config = true,
-    },
-    {
-        "echasnovski/mini.ai",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            n_lines = 500,
-        },
-        config = function(_, opts)
-            require("mini.ai").setup(opts)
-        end,
-    },
-    {
-        "echasnovski/mini.bracketed",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            comment = { suffix = "z" },
-        },
-        config = true,
-    },
-    {
-        "echasnovski/mini.comment",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                custom_commentstring = nil,
-                ignore_blank_line = true,
-            },
-        },
-        config = true,
-    },
-    {
-        "echasnovski/mini.surround",
-        version = "*",
-        event = "VeryLazy",
-        opts = {
-            custom_surroundings = {
-                ["("] = { input = { "%b()", "^.().*().$" }, output = { left = "(", right = ")" } },
-                ["["] = { input = { "%b[]", "^.().*().$" }, output = { left = "[", right = "]" } },
-                ["{"] = { input = { "%b{}", "^.().*().$" }, output = { left = "{", right = "}" } },
-                ["<"] = { input = { "%b<>", "^.().*().$" }, output = { left = "<", right = ">" } },
-            },
-        },
-        config = true,
-    },
-    {
-        "echasnovski/mini.cursorword",
-        version = "*",
-        event = "VeryLazy",
-        init = function()
-            vim.api.nvim_create_autocmd("User", {
-                pattern = "MiniFilesWindowOpen",
-                callback = function(args)
-                    local win_id = args.data.win_id
-                    vim.wo[win_id].number = false
-
-                    -- Customize window-local settings
-                    local config = vim.api.nvim_win_get_config(win_id)
-                    config.border = "rounded"
-                    config.title_pos = "center"
-                    vim.api.nvim_win_set_config(win_id, config)
-                end,
-            })
-        end,
-        config = true,
-    },
-    {
-        "echasnovski/mini.pairs",
-        version = "*",
-        event = "VeryLazy",
-        config = true,
-    },
-    {
-        "echasnovski/mini.files",
-        version = "*",
-        event = "VeryLazy",
+        "echasnovski/mini.nvim",
+        version = false,
         keys = {
             {
                 "<leader>e",
@@ -127,28 +29,94 @@ return {
                 mode = "n",
             },
         },
-        init = file_functions.init,
-        opts = {
-            options = {
-                permanent_delete = true,
-                use_as_default_explorer = true,
-            },
-            windows = {
-                preview = true,
-                width_preview = 80,
-                width_focus = 50,
-                width_nofocus = 20,
-            },
-            content = {
-                filter = file_functions.filter,
-                sort = file_functions.sort,
-            },
-            -- mappings = {
-            --     go_in = "l",
-            --     go_in_plus = "L",
-            --     go_out = "h",
-            --     go_out_plus = "H",
-            -- }
-        },
+        init = function()
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesWindowOpen",
+                callback = function(args)
+                    local win_id = args.data.win_id
+                    vim.wo[win_id].number = false
+
+                    -- Customize window-local settings
+                    local config = vim.api.nvim_win_get_config(win_id)
+                    config.border = "rounded"
+                    config.title_pos = "center"
+                    vim.api.nvim_win_set_config(win_id, config)
+                end,
+            })
+        end,
+        config = function()
+            require("mini.splitjoin").setup()
+            require("mini.move").setup({
+                mappings = {
+                    left = "<M-h>",
+                    right = "<M-l>",
+                    down = "<M-j>",
+                    up = "<M-k>",
+
+                    line_left = "<M-h>",
+                    line_right = "<M-l>",
+                    line_down = "<M-j>",
+                    line_up = "<M-k>",
+                },
+            })
+            require("mini.ai").setup({
+                n_lines = 500,
+            })
+            require("mini.bracketed").setup({
+                comment = {
+                    suffix = "z",
+                },
+            })
+            require("mini.comment").setup({
+                options = {
+                    custom_commentstring = nil,
+                    ignore_blank_line = true,
+                },
+            })
+            require("mini.surround").setup({
+                custom_surroundings = {
+                    ["("] = {
+                        input = { "%b()", "^.().*().$" },
+                        output = { left = "(", right = ")" },
+                    },
+                    ["["] = {
+                        input = { "%b[]", "^.().*().$" },
+                        output = { left = "[", right = "]" },
+                    },
+                    ["{"] = {
+                        input = { "%b{}", "^.().*().$" },
+                        output = { left = "{", right = "}" },
+                    },
+                    ["<"] = {
+                        input = { "%b<>", "^.().*().$" },
+                        output = { left = "<", right = ">" },
+                    },
+                },
+            })
+            require("mini.cursorword").setup()
+            require("mini.pairs").setup()
+            require("mini.files").setup({
+                options = {
+                    permanent_delete = true,
+                    use_as_default_explorer = true,
+                },
+                windows = {
+                    preview = true,
+                    width_preview = 80,
+                    width_focus = 50,
+                    width_nofocus = 20,
+                },
+                content = {
+                    filter = file_functions.filter,
+                    sort = file_functions.sort,
+                },
+                -- mappings = {
+                --     go_in = "l",
+                --     go_in_plus = "L",
+                --     go_out = "h",
+                --     go_out_plus = "H",
+                -- }
+            })
+        end,
     },
 }
