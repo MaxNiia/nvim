be5b376157c4beb8db352815b7589c5c9309f6c6
Max Niia <max.emil.niia@gmail.com>
2025-04-07 21:21:05 +0200

    feat: stop seesions on exiting insert mode

--- lua/niia/plugins/mini/init.lua
+++ lua/niia/plugins/mini/init.lua
@@ -64,6 +64,19 @@ return {
                     stop = "",
                 },
             })
+
+            local make_stop = function()
+                local au_opts = { pattern = "*:n", once = true }
+                au_opts.callback = function()
+                    while MiniSnippets.session.get() do
+                        MiniSnippets.session.stop()
+                    end
+                end
+                vim.api.nvim_create_autocmd("ModeChanged", au_opts)
+            end
+            local opts = { pattern = "MiniSnippetsSessionStart", callback = make_stop }
+            vim.api.nvim_create_autocmd("User", opts)
+
             require("mini.splitjoin").setup()
             require("mini.icons").setup({
                 file = {
