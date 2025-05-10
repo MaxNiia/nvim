a9571c95298168c540674861f16daad6fcf18f86
Max Niia <max.emil.niia@gmail.com>
2025-05-10 22:45:08 +0200

    feat: turn off word_diff

--- lua/niia/plugins/gitsigns.lua
+++ lua/niia/plugins/gitsigns.lua
@@ -27,7 +27,7 @@ return {
             signcolumn = true,
             numhl = true,
             linehl = false,
-            word_diff = true,
+            word_diff = false,
             watch_gitdir = {
                 follow_files = true,
             },
