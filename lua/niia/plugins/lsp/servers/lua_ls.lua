9ea9e9109ab5491a3ab1964bb5d3cfe9108b30d5
Max Niia <max.emil.niia@gmail.com>
2025-05-17 17:26:57 +0200

    fix: remove luals looking at every single file

--- lua/niia/plugins/lsp/servers/lua_ls.lua
+++ lua/niia/plugins/lsp/servers/lua_ls.lua
@@ -20,7 +20,7 @@ return {
             },
             workspace = {
                 checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true),
+                -- library = vim.api.nvim_get_runtime_file("", true),
             },
         },
     },
