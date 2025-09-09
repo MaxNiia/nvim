1e25215355e677893786aa0b3c9d5419668fb77e
Max Niia <max.emil.niia@gmail.com>
2025-09-09 23:11:09 +0200

    feat: minimize lualine configuration and remove unnecesary clutter

--- lua/niia/plugins/lualine.lua
+++ lua/niia/plugins/lualine.lua
@@ -1,133 +1,9 @@
-local status = ""
-
-local getActiveModeColor = function()
-    local mode_color = {
-        n = "lualine_a_normal",
-        i = "lualine_a_insert",
-        v = "lualine_a_visual",
-        [""] = "lualine_a_visual",
-        V = "lualine_a_visual",
-        no = "lualine_a_normal",
-        s = "lualine_a_visual",
-        S = "lualine_a_visual",
-        [""] = "lualine_a_visual",
-        ic = "lualine_a_normal",
-        R = "lualine_a_replace",
-        Rv = "lualine_a_replace",
-        c = "lualine_a_command",
-        cv = "lualine_a_command",
-        ce = "lualine_a_command",
-        r = "lualine_a_command",
-        rm = "lualine_a_command",
-        ["r?"] = "lualine_a_command",
-        ["!"] = "lualine_a_command",
-        t = "lualine_a_terminal",
-    }
-    local mode = vim.fn.mode()
-    return mode_color[mode]
-end
-
-local getInactiveModeColor = function()
-    local mode_color = {
-        n = "lualine_b_normal",
-        i = "lualine_b_insert",
-        v = "lualine_b_visual",
-        [""] = "lualine_b_visual",
-        V = "lualine_b_visual",
-        no = "lualine_b_normal",
-        s = "lualine_b_visual",
-        S = "lualine_b_visual",
-        [""] = "lualine_b_visual",
-        ic = "lualine_b_normal",
-        R = "lualine_b_replace",
-        Rv = "lualine_b_replace",
-        c = "lualine_b_command",
-        cv = "lualine_b_command",
-        ce = "lualine_b_command",
-        r = "lualine_b_command",
-        rm = "lualine_b_command",
-        ["r?"] = "lualine_b_command",
-        ["!"] = "lualine_b_command",
-        t = "lualine_b_terminal",
-    }
-    local mode = vim.fn.mode()
-    return mode_color[mode]
-end
-
-local getStatusLineColor = function()
-    local mode_color = {
-        n = "lualine_m_normal",
-        i = "lualine_m_insert",
-        v = "lualine_m_visual",
-        [""] = "lualine_m_visual",
-        V = "lualine_m_visual",
-        no = "lualine_m_normal",
-        s = "lualine_m_visual",
-        S = "lualine_m_visual",
-        [""] = "lualine_m_visual",
-        ic = "lualine_m_normal",
-        R = "lualine_m_replace",
-        Rv = "lualine_m_replace",
-        c = "lualine_m_command",
-        cv = "lualine_m_command",
-        ce = "lualine_m_command",
-        r = "lualine_m_command",
-        rm = "lualine_m_command",
-        ["r?"] = "lualine_m_command",
-        ["!"] = "lualine_m_command",
-        t = "lualine_m_terminal",
-    }
-    local mode = vim.fn.mode()
-    return mode_color[mode]
-end
-
 local function macro_recording()
     local mode = require("noice").api.statusline.mode.get()
     if mode then
         return string.match(mode, "^recording @.*") or ""
     end
     return ""
-end
-
-local getWinbarConfig = function(active)
-    return {
-        {
-            "%=",
-            padding = 0,
-        },
-        {
-            "diff",
-            separator = { left = "", right = "" },
-            color = getInactiveModeColor,
-        },
-        {
-            "filetype",
-            icon = { align = "left" },
-            colored = true,
-            icon_only = true,
-            color = getInactiveModeColor,
-            padding = 0,
-            separator = { left = "", right = "" },
-        },
-        {
-            "filename",
-            newfile_status = true,
-            path = 1,
-            separator = { left = "", right = "" },
-            color = active and getActiveModeColor or getInactiveModeColor,
-            padding = 0,
-        },
-        {
-            "diagnostics",
-            sources = { "nvim_diagnostic" },
-            separator = { left = "", right = "" },
-            color = getInactiveModeColor,
-        },
-        {
-            "%=",
-            padding = 0,
-        },
-    }
 end
 
 return {
@@ -153,8 +29,8 @@ return {
                 icons_enabled = true,
                 theme = "auto",
                 component_separators = {
-                    left = "", --"",
-                    right = "", --"",
+                    left = "",
+                    right = "",
                 },
                 section_separators = {
                     left = "",
@@ -165,11 +41,24 @@ return {
                         "dashboard",
                         "lazy",
                         "Mason",
+                        "spectre_panel",
+                        "Outline",
+                        "trouble",
+                        "dapui_scopes",
+                        "dapui_breakpoints",
+                        "dapui_stacks",
+                        "dapui_console",
+                        "dapui_watches",
+                        "dapui_scopes",
+                        "dap-repl",
+                        "dashboard",
+                        "terminal",
+                        "lazy",
                     },
                     winbar = {
                         "spectre_panel",
                         "Outline",
-                        "Trouble",
+                        "trouble",
                         "dapui_scopes",
                         "dapui_breakpoints",
                         "dapui_stacks",
@@ -184,7 +73,7 @@ return {
                 },
                 ignore_focus = {},
                 always_divide_middle = false,
-                always_show_tabline = true,
+                always_show_tabline = false,
                 globalstatus = vim.o.laststatus == 3,
                 refresh = {
                     statusline = 100,
@@ -200,27 +89,16 @@ return {
                 "mason",
                 "trouble",
             },
-            inactive_winbar = {
+            inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = getWinbarConfig(false),
+                lualine_c = {},
                 lualine_x = {},
                 lualine_y = {},
                 lualine_z = {},
             },
-            winbar = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = getWinbarConfig(true),
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {},
-            },
-            -- Statusline
             sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {
+                lualine_a = {
                     {
                         function()
                             local cwd = vim.uv.cwd()
@@ -228,94 +106,130 @@ return {
                             cwd, _ = cwd:gsub(home, "~")
                             return cwd
                         end,
-                        padding = 0,
-                        color = getActiveModeColor,
-                        separator = { left = "", right = "" },
                     },
-                    {
-                        "copilot",
-                        cond = vim.g.copilot,
-                        color = getInactiveModeColor,
-                        separator = { left = "", right = "" },
-                    },
-                    {
-                        "diagnostics",
-                        sources = { "nvim_workspace_diagnostic" },
-                        separator = { left = "", right = "" },
-                        color = getInactiveModeColor,
-                    },
-                    {
-                        "%=",
-                        padding = 0,
-                        color = getStatusLineColor,
-                    },
+                },
+                lualine_b = {
                     {
                         "b:gitsigns_head",
                         icon = "",
-                        separator = { left = "", right = "" },
-                        color = getActiveModeColor,
-                        padding = 0,
+                    },
+                },
+                lualine_c = {
+                    {
+                        "copilot",
+                        cond = vim.g.copilot,
                     },
                     {
-                        "%=",
-                        padding = 0,
-                        color = getStatusLineColor,
+                        function()
+                            return "  " .. require("dap").status()
+                        end,
+                        cond = function()
+                            return package.loaded["dap"] and require("dap").status() ~= ""
+                        end,
                     },
+                    {
+                        require("lazy.status").updates,
+                        cond = require("lazy.status").has_updates,
+                    },
+                },
+                lualine_x = {
                     {
                         macro_recording,
                         cond = function()
                             return package.loaded["noice"]
                                 and require("noice").api.status.mode.has()
                         end,
-                        color = getInactiveModeColor,
-                        separator = { left = "", right = "" },
                     },
                     {
-                        function()
-                            return require("noice").api.status.command.get()
-                        end,
-                        cond = function()
-                            return package.loaded["noice"]
-                                and require("noice").api.status.command.has()
-                        end,
-                        color = getInactiveModeColor,
-                        separator = { left = "", right = "" },
+                        "searchcount",
+                        maxcount = 999,
+                        timeout = 500,
                     },
                     {
                         "progress",
-                        color = getInactiveModeColor,
-                        separator = { left = "", right = "" },
                     },
                     {
                         "location",
-                        color = getInactiveModeColor,
-                        separator = { left = "", right = "" },
                     },
+                },
+                lualine_y = {
+                    {
+                        "datetime",
+                        style = "%H:%M",
+                    },
+                },
+                lualine_z = {
                     {
                         "tabs",
                         mode = 2,
                         path = 0,
-                        use_mode_colors = false,
                         show_modified_status = true,
-                        tabs_color = {
-                            active = getActiveModeColor,
-                            inactive = getInactiveModeColor,
-                        },
-                        padding = 0,
                         symbols = {
                             modified = "",
                         },
-                        separator = { left = "", right = "" },
+                    },
+                },
+            },
+            inactive_winbar = {
+                lualine_a = {},
+                lualine_b = {
+                    {
+                        "filetype",
+                        icon = { align = "right" },
+                        padding = { left = 1, right = 0 },
+                        colored = true,
+                        icon_only = true,
                     },
                     {
-                        "datetime",
-                        style = "%H:%M",
-                        color = getInactiveModeColor,
-                        padding = {
-                            right = 0,
-                            left = 1,
-                        },
-                        separator = { left = "", right = "" },
+                        "filename",
+                        path = 1,
+                        newfile_status = true,
+                        padding = { left = 0, right = 1 },
+                    },
+                    {
+                        "fileformat",
+                    },
+                },
+                lualine_c = {
+                    {
+                        "diagnostics",
+                        sources = { "nvim_diagnostic" },
+                    },
+                    {
+                        "diff",
+                    },
+                },
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+            winbar = {
+                lualine_a = {},
+                lualine_b = {
+                    {
+                        "filetype",
+                        icon = { align = "right" },
+                        padding = { left = 1, right = 0 },
+                        colored = true,
+                        icon_only = true,
+                    },
+                    {
+                        "filename",
+                        padding = { left = 0, right = 1 },
+                        newfile_status = true,
+                        path = 1,
+                    },
+                    {
+                        "fileformat",
+                    },
+                },
+                lualine_c = {
+                    {
+                        "diagnostics",
+                        sources = { "nvim_diagnostic" },
+                    },
+                    {
+                        "diff",
                     },
                 },
                 lualine_x = {},
