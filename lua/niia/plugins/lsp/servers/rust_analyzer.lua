8cc970032e4d565bc916c26d20903703d7854d07
Max <40739953+MaxNiia@users.noreply.github.com>
2025-09-30 19:35:08 +0200

    Fix rust analyzer procMacro setting

--- lua/niia/plugins/lsp/servers/rust_analyzer.lua
+++ lua/niia/plugins/lsp/servers/rust_analyzer.lua
@@ -12,7 +12,7 @@ return {
                     enable = true,
                 },
             },
-            proMacro = {
+            procMacro = {
                 enable = true,
             },
         },
