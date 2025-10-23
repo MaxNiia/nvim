2e9f345d805972c40ce113babd48ed1b57aa7c90
Max Niia <max.emil.niia@gmail.com>
2025-10-23 20:02:41 +0200

    feat: remove git config handling

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
-if
-    vim.loop.os_uname().sysname == "Linux"
-    and os.getenv("USER") == "max"
-    and vim.fn.getcwd() == vim.fn.expand("$HOME")
-then
-    vim.env.GIT_DIR = vim.fn.expand("$HOME/.cfg")
-    vim.env.GIT_WORK_TREE = vim.fn.expand("$HOME")
-    vim.g.fugitive_git_executable = "/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME"
-end
-
 if vim.g.vscode == nil then
     vim.g.vscode = false
 end
